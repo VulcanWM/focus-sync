9503ffde803ace85a03cda3b91370bf0e762ace1
VulcanWM <logmedha09@gmail.com>
2023-09-19 18:07:50 +0100

    added houses to create account form

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -8,6 +8,24 @@ export default function Home() {
         <h2 className={styles.small_title}>create account</h2>
         <input placeholder="username" className={styles.signup_input} required></input><br/>
         <textarea className={styles.signup_textarea} required>your goal...</textarea><br/><br/>
+        <ul className={styles.houses}>
+          <li className={styles.verdant}>
+            <input type="radio" id="verdant" name="house" />
+            <label htmlFor="verdant">Verdant</label>
+          </li>
+          <li className={styles.lumos}>
+            <input type="radio" id="lumos" name="house" />
+            <label htmlFor="lumos">Lumos</label>
+          </li>
+          <li className={styles.erythro}>
+            <input type="radio" id="erythro" name="house"/>
+            <label htmlFor="erythro">Erythro</label>
+          </li>
+          <li className={styles.azurite}>
+            <input type="radio" id="azurite" name="house" />
+            <label htmlFor="azurite">Azurite</label>
+          </li>
+        </ul>
         <button className={styles.signup_button}>create account</button>
       </div>
     </Layout>
