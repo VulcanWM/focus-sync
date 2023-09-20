1e0aeff329325d9a0a08e089bdc83026ec78361d
VulcanWM <logmedha09@gmail.com>
2023-09-20 18:54:03 +0100

    checking if user already exists

--- lib/database.ts
+++ lib/database.ts
@@ -1,14 +1,35 @@
 import dbConnect from './mongodb'
 import User from '../models/User'
 
+export async function get_user(username: String) {
+    await dbConnect();
+    const user = await User.find({username: username})
+    if (user.length == 0){
+        return false
+    } else {
+        return user[0]
+    }
+}
+
+export async function get_user_from_email(email: String) {
+    await dbConnect();
+    const user = await User.find({email: email})
+    if (user.length == 0){
+        return false
+    } else {
+        return user[0]
+    }
+}
+
 export async function create_user(username: String, email: String, goal: String, house: String) {
     await dbConnect();
-
     username = username.toLowerCase();
-
-    // check if email exists
-    // check if username exists
-
+    if (await get_user(username) != false){
+        return "This username already exists!"
+    }
+    if (await get_user_from_email(email) != false){
+        return "This email is already being used for an account!"
+    }
     const user = await User.create({username: username, email: email, goal: goal})
     return user
 }
