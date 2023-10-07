3294355a0f9bb4520e1cf5e2866540378bb2e1da
VulcanWM <logmedha09@gmail.com>
2023-10-07 17:36:23 +0100

    edited landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,11 @@ export default function Home() {
     <Layout pageTitle="Home">
       <div id="middle">
         <h1 className={styles.title}>FocusSync</h1>
+        <p>community driven productivity website that makes achieving your goals social and fun</p>
         <ul>
-          <li className={styles.point}>Be accountable: Join a group for days of focus, motivation and discipline.</li>
-          <li className={styles.point}>Get support: Encouragement and advice from others walking the same path.</li>
-          <li className={styles.point}>Learn faster: Succeed together and grow from each other's experiences.</li>
+          <li className={styles.point}>Set a goal</li>
+          <li className={styles.point}>Track the time you spend on each task</li>
+          <li className={styles.point}>Stay motivated by seeing how others are performing</li>
         </ul>
         <button onClick={redirectToJoin} className={styles.join_button}><span className={styles.join_link}>join us</span></button>
       </div>
