0ab5f5fa6f34d385cc493e200fdb83386c19cac0
VulcanWM <logmedha09@gmail.com>
2023-10-31 22:14:07 +0000

    added close milestone api route

---
+++ pages/api/close-milestone.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { close_milestone, get_user_from_email } from '@/lib/database';
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./auth/[...nextauth]"
+
+type Data = {
+  message: string,
+  error: boolean
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const session = await getServerSession(req, res, authOptions)
+  if (session) {
+    const name = req.body.name as string;
+    const user = await get_user_from_email(session!.user!.email as string);
+    const username = user.username;
+    const func = await close_milestone(name, username)
+    if (func == true){
+      res.status(200).json({ message: 'Closed!', error: false })
+    } else {
+      res.status(200).json({ message: func, error: true })
+    }
+  } else {
+    res.status(200).json({ message: "Login first!", error: true })
+  }
+}
